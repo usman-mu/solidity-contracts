ee44ce5aa5454e2b15b1c3769c5c3013b4e85307
Muhammad Usman <usmancou99@gmail.com>
2022-02-04 12:46:21 +0200

    Update Lottery.sol

--- Lottery.sol
+++ Lottery.sol
@@ -11,29 +11,65 @@ contract Lottery {
         lotteryId = 1;
         owner = msg.sender;
     }
+
+    /**
+    * @dev sets minimum amount of contribution to participate in lottery
+    */
     function setAmountToParticipate(uint amount) public  ownerOnly {
         amountToParticipate = amount;
     }
+
+    /**
+    * @dev returns individual winner of each lottery by lottery ID
+    */
     function getWinnerByLottery(uint lottery) public view returns(address payable){
         return lotteryHistory[lottery];
     }
+
+    /**
+    * @dev returns total funds participated in current lottery
+    */
     function getBalance() public view returns(uint){
         return address(this).balance;
     }
+
+    /**
+    * @dev gives list of all participants in current lottery
+    */
     function getParticipants() public view returns(address payable[] memory){
         return participants;
     }
+
+    /**
+    * @dev checks if participant sent enough funds to participate
+    * and adds them to participants list
+    */
     function enter() public payable {
         require(msg.value >= amountToParticipate);
         participants.push(payable(msg.sender));
     }
+
+    /**
+    * @dev generates random number
+    */
     function getRandomNumber() public view returns(uint){
         return uint(keccak256(abi.encodePacked(owner, block.timestamp)));
     }
+
+    /**
+    * @dev makes sure caller is the owner of contract
+    */
     modifier ownerOnly(){
         require(msg.sender == owner);
         _;
     }
+
+    /**
+     * @dev Picks winner and total contributed amount to winner.
+     * adds winner to history
+     * increases lottery id
+     * resets participants array
+     */
     function pickWinner() public ownerOnly{
         uint index = getRandomNumber() % participants.length;
         participants[index].transfer(address(this).balance);
